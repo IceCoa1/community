1ab4ce188916c2e4ca05ca192650a9879bb1db8c
MaJinming <mjm815@126.com>
2020-03-25 10:58:07 +0800

    first commit

---
+++ src/main/java/top/icecoal/community/controlller/Hello.java
@@ -0,0 +1,12 @@
+package top.icecoal.community.controlller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Hello {
+    @GetMapping("/")
+    public String hello(){
+        return "hello";
+    }
+}
